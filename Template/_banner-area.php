3361b5e2e29998befb24b9b0bd6fc4e5f2df5ad7
Akshat Jain <aksjain891999@gmail.com>
2020-08-16 12:41:48 +0530

    update

--- Template/_banner-area.php
+++ Template/_banner-area.php
@@ -5,7 +5,7 @@
             <a href="product.php?item_id=5"><img src="./assets/Banner1.png" alt="Banner1"></a>
         </div>
         <div class="item">
-            <a href="product.php?item_id=15"><img src="./assets/Banner3.png" alt="Banner2"></a>
+            <a href="product.php?item_id=15"><img src="./assets/Banner2.png" alt="Banner2"></a>
         </div>
         <div class="item">
             <a href="product.php?item_id=14"><img src="./assets/Banner3.jpg" alt="Banner3"></a>
